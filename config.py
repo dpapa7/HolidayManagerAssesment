96c0098c4d0e85fe4f054f215db4035781f24f7e
dpapa7 <dypapadopoulos@gmail.com>
2022-07-11 11:29:00 -0400

    final commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 saveJsonLoc = 'SavedHolidays.json'
 loadJsonLoc = 'Assesments/HolidayManagerAssesment/holidays.json'
-# scrapeURL = f'https://www.timeanddate.com/holidays/us/{year}?hol=33554809'
+# apikey = ''
